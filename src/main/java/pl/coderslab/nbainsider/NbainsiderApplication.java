cdd76ee89ec647b02af01751865f00f166c392ce
Araggorn <zerelik@gmail.com>
2020-11-28 09:31:56 +0100

    startofproject

---
+++ src/main/java/pl/coderslab/nbainsider/NbainsiderApplication.java
@@ -0,0 +1,13 @@
+package pl.coderslab.nbainsider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbainsiderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NbainsiderApplication.class, args);
+    }
+
+}
