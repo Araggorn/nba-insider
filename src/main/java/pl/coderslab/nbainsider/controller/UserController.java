2e3bc7dc722ec6601371b67ea2139e623af54057
Araggorn <zerelik@gmail.com>
2020-12-09 17:56:56 +0100

    validation

--- src/main/java/pl/coderslab/nbainsider/controller/UserController.java
+++ src/main/java/pl/coderslab/nbainsider/controller/UserController.java
@@ -3,6 +3,7 @@ package pl.coderslab.nbainsider.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +12,7 @@ import pl.coderslab.nbainsider.dto.UserItemDto;
 import pl.coderslab.nbainsider.entity.User;
 import pl.coderslab.nbainsider.service.UserService;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -36,7 +38,8 @@ public class UserController {
     }
 
     @PostMapping("/registration")
-    public String add(UserDto user) {
+    public String add(@Valid UserDto user, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {return "registration";}
         userService.add(user);
         return "redirect:/userlist";
     }
