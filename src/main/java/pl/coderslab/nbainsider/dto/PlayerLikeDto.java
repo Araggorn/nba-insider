f5520dda8df51c99f161dd5a148da7b3a8826697
Araggorn <zerelik@gmail.com>
2021-02-22 00:30:38 +0100

    xcz

--- src/main/java/pl/coderslab/nbainsider/dto/PlayerLikeDto.java
+++ src/main/java/pl/coderslab/nbainsider/dto/PlayerLikeDto.java
@@ -2,9 +2,11 @@ package pl.coderslab.nbainsider.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class PlayerLikeDto {
     private int counter;
     private Long id;
