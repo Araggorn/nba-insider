3d5d41792f3b8a61d67ab9b9d22dd4bb7d568907
Araggorn <zerelik@gmail.com>
2021-03-03 15:35:59 +0100

    Cleaning mess part 2

--- src/test/java/pl/coderslab/nbainsider/list/MyLinkedList.java
+++ src/test/java/pl/coderslab/nbainsider/list/MyLinkedList.java
@@ -1,24 +1,24 @@
-package pl.coderslab.nbainsider.list;
-
-import com.google.common.base.MoreObjects;
-
-
-public class MyLinkedList implements MyList {
-
-    private Node first = null;
-
-    private static class Node {
-        Node next;
-        Node previous;
-        String value;
-
-        @Override
-        public String toString() {
-            return MoreObjects.toStringHelper(this)
-                    .add("value", value)
-                    .toString();
-        }
-    }
+//package pl.coderslab.nbainsider.list;
+//
+//import com.google.common.base.MoreObjects;
+//
+//
+//public class MyLinkedList implements MyList {
+//
+//    private Node first = null;
+//
+//    private static class Node {
+//        Node next;
+//        Node previous;
+//        String value;
+//
+//        @Override
+//        public String toString() {
+//            return MoreObjects.toStringHelper(this)
+//                    .add("value", value)
+//                    .toString();
+//        }
+//    }
 
     @Override
     public String toString() {
