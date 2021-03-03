4ff9c4c8204a0168358617590e3761a8c9e0c18e
Araggorn <zerelik@gmail.com>
2021-03-03 22:20:15 +0100

    Checking something

--- src/main/java/pl/coderslab/nbainsider/app/SecurityUtils.java
+++ src/main/java/pl/coderslab/nbainsider/app/SecurityUtils.java
@@ -4,6 +4,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 
 public class SecurityUtils {
 
+
     public static String username() {
         return SecurityContextHolder.getContext().getAuthentication().getName();
     }
